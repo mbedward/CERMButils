a168f4af98ca15e333f3f5a20443babd613027b5
mbedward <michael.bedward@gmail.com>
2024-08-26 18:12:48 +1000

    fix bugs and deal with messy input geometries

--- R/make_progressions.R
+++ R/make_progressions.R
@@ -8,24 +8,54 @@
 #'   and polygonal features representing the total extent of a fire at each time.
 #'
 #' @param time_cols A character vector of one or more column names that define
-#'   the time steps for the progressions.
+#'   the time steps for the progressions. Normally a time column will will hold
+#'   a Date or Date-Time value (e.g. POSIX.ct) but any scalar variable type that
+#'   can be sorted is acceptable. When two or more time columns are specified,
+#'   sorting will be based on the provided order of column names.
+#'
+#' @param out_epsg Integer EPSG code specifying the map projection for the
+#'   output progression polygons. Default is 8058 (NSW Lambert / GDA2020).
 #'
 #' @param min_geom_area Threshold minimum area (square metres) for progression
 #'   polygons. If the progression for a given time step consists of two or more
 #'   spatially distinct parts, the threshold is applied to each individually.
+#'
+#' @param replicate_times A single string that specifies what to do if there is
+#'   more than one input data record for any time step. Options are:
+#'   \code{'largest'} (choose the record with the largest extent polygon area);
+#'   \code{'smallest'} (choose the record with the largest extent polygon area);
+#'   \code{'fail'} (stop with an error message). The default is
+#'   \code{'largest'}. The argument may be abbreviated and is case-sensitive.
+#'
+#' @param dTolerance Distance tolerance used to simplify the input extent
+#'   polygons using the \code{\link[sf]{st_simplify}} function. Default is 2 for
+#'   a minimum between vertex distance of 2 metres. Set to 0 to omit the
+#'   simplification step.
 #'
 #' @return An \code{sf} spatial data frame of progression polygons with
 #'   attribute values copied from the input data.
 #'
 #' @export
 #'
-make_progressions <- function(x, time_cols, min_geom_area = 100) {
+make_progressions <- function(x,
+                              time_cols,
+                              out_epsg = 8058,
+                              min_geom_area = 100,
+                              replicate_times = c('largest', 'smallest', 'fail'),
+                              dTolerance = 2) {
+
   checkmate::assert_class(x, "sf")
 
   the_crs <- sf::st_crs(x)
   if (is.na(the_crs)) {
     stop("The input sf data frame must have a coordinate reference system defined")
   }
+
+  checkmate::assert_integerish(out_epsg, any.missing = FALSE, len = 1)
+  x <- sf::st_transform(x, out_epsg)
+
+  checkmate::assert_number(dTolerance, lower = 0)
+  if (dTolerance > 0) x <- sf::st_simplify(x, dTolerance = dTolerance)
 
   checkmate::assert_character(time_cols, min.len = 1, any.missing = FALSE)
   ok <- time_cols %in% colnames(x)
@@ -35,25 +65,55 @@ make_progressions <- function(x, time_cols, min_geom_area = 100) {
     stop(msg)
   }
 
-  # Check that times are unique
-  xtimes <- apply(x[, time_cols], 1, paste, collapse=" ")
+  checkmate::assert_number(min_geom_area, lower = 0)
+
+  replicate_times <- match.arg(replicate_times)
+
+  # Subset the input data to records with geometrically distinct features
+  # gunique <- unique(sf::st_geometry(x)) %>%
+  #   sf::st_as_sfc()
+  #
+  # sf::st_crs(gunique) <- sf::st_crs(x)
+  # ii <- match(gunique, sf::st_geometry(x))
+  # x <- x[ii, ]
+
+  # Check whether times are unique
+  xtimes <- apply(sf::st_drop_geometry(x[, time_cols]), 1, paste, collapse=" ")
   ntimes <- table(xtimes)
 
-  if (any(ntimes) > 1) {
-    msg <- paste(time_cols, collapse = ", ")
-    msg <- glue::glue("One or more times defined by {msg} occur more than once")
+  if (any(ntimes > 1)) {
+    if (replicate_times == "fail") {
+      msg <- paste(time_cols, collapse = ", ")
+      msg <- glue::glue("One or more times defined by {msg} occur more than once
+                         and replicate_times was set to 'fail'")
+      stop(msg)
+
+    } else {
+      # Choose one record per time based on the largest or smallest feature area
+      a <- as.numeric( sf::st_area(x) )
+
+      x <- x %>%
+        dplyr::mutate(.area_ = a) %>%
+        dplyr::group_by(dplyr::across(dplyr::all_of(time_cols)))
+
+      if (replicate_times == "largest") {
+        x <- dplyr::filter(x, .area_ == max(.area_))
+      } else {
+        x <- dplyr::filter(x, .area_ == min(.area_))
+      }
+
+      # If there are identical feature geometries in any time step there
+      # will still be multiple records per time, so just choose the first
+      x <- dplyr::slice_head(x, n = 1)
+
+      x <- x %>%
+        dplyr::ungroup() %>%
+        dplyr::select(-.area_)
+    }
   }
 
   # Sort data by time
   x <- dplyr::arrange(x, dplyr::across(dplyr::all_of(time_cols)))
-
-  # Subset the input data to records with geometrically distinct features
-  gunique <- unique(sf::st_geometry(x)) %>%
-    sf::st_as_sfc()
-
-  sf::st_crs(gunique) <- sf::st_crs(x)
-  ii <- match(gunique, sf::st_geometry(x))
-  x <- x[ii, ]
 
   # Initialize the outer fire extent
   gouter <- sf::st_geometry(x[1,])
@@ -69,18 +129,32 @@ make_progressions <- function(x, time_cols, min_geom_area = 100) {
     # an `sfc` geometry list object with the parent CRS applied. This
     # ensures that st_area will provide values in m^2 (below).
     gnew <- sf::st_difference(g, gouter) %>%
-      sf::st_sfc(., crs = sf::st_crs(x))
+      sf::st_sfc(., crs = sf::st_crs(x)) %>%
+      sf::st_make_valid()
 
-    # Update outer extent so far
-    gouter <<- sf::st_union(gouter, g)
+    # Discard any non-polygonal geometries and any polygons with area
+    # less than the minimum threshold.
+    is_poly <- sf::st_is(gnew, c("POLYGON", "MULTIPOLYGON"))
+    if (any(is_poly)) {
+      gnew <- gnew[is_poly]
 
-    if (length(gnew) > 0) {
-      # check for any non-polygonal geometries or tiny slivers
+      # Convert any multipolygons to polygons
       gnew <- sf::st_cast(gnew, "POLYGON")
-      a <- as.numeric( sf::st_area(gnew) )
-      res <- gnew[a >= min_geom_area]
-    }
 
+      # Check area
+      a <- as.numeric( sf::st_area(gnew) )
+      ok <- a >= min_geom_area
+      if (any(ok)) {
+        gnew <- gnew[ok]
+
+        # Update outer extent so far
+        gouter <<- sf::st_union(gouter, gnew) %>%
+          sf::st_union() %>%  # second call to dissolve internal boundaries
+          sf::st_make_valid()
+
+        res <- sf::st_cast(gnew, "POLYGON")
+      }
+    }
     res
   })
 
